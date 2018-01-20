c80af9e1c687ca0a4a6797a0c133c46ed0525bb0
“michal-adamczyk8 <adamczyk888@gmail.com>
2018-01-20 15:54:00 +0100

    Zadanie 7.2

--- src/main/java/com/kodilla/stream/StreamMain.java
+++ src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.referance.FunctionalCalculator;
 
@@ -14,5 +15,8 @@ public class StreamMain {
         poemBeautifier.beutify("Michał Adamczyk", textToBeautify -> textToBeautify.toLowerCase());
         poemBeautifier.beutify("Michał Adamczyk", textToBeautify -> "Imię: " + textToBeautify.substring(0, textToBeautify.indexOf(" "))+ " Nazwisko: " + textToBeautify.substring(textToBeautify.indexOf(" ") + 1,textToBeautify.length()));
         poemBeautifier.beutify(("Michał Adamczyk"), textToBeautify -> new StringBuilder(textToBeautify).reverse().toString());
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
